f834f30a1202bfda266a4df40d4847eda7883410
chungdana <146171173+chungdana@users.noreply.github.com>
2023-09-27 16:38:50 +0900

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+//1. string concatenation
+console.log('my' + 'cat');
+console.log('1' + 2);
+console.log('string literals: 1+2=$(1+2)')
+
+//2. Numeric operators
+console.log(1 + 1);//add
+console.log(1 + 1);//substract
+console.log(1 + 1);//divide
+console.log(1 + 1);//multiply
+console.log(1 + 1);//remainder
+console.log(1 + 1);//exponentiation
+
+//5. comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+//&& (and), finds the first falsy value
+console.log('and:s{value1 && value2 && check()}');
+
+function check() {
+    for (let i = 0; i < 10; i++) {
+        //wasting time
+        console.log('ÀÓÆ¼')
+    }
+    return true
+}
